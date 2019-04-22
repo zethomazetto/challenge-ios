dd6594169f26af874bdcc25c30d40c58ad68447d
Ezequiel Thomazetto <zethomazetto@MacBook-Pro-de-Ezequiel.local>
2019-04-22 14:07:06 -0300

    project init

---
+++ AmeDigital-challenge-ios/AmeDigital-challenge-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmeDigital-challenge-ios
+//
+//  Created by Ezequiel Thomazetto on 22/04/19.
+//  Copyright © 2019 Ezequiel Thomazetto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
