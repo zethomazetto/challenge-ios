efec47857c6e188764fd50ba12e9950d728e9686
Ezequiel Thomazetto <zethomazetto@MacBook-Pro-de-Ezequiel.local>
2019-04-30 08:21:55 -0300

    project commit

--- AmeDigital-challenge-ios/Pods/Target Support Files/Pods-AmeDigital-challenge-ios/Pods-AmeDigital-challenge-ios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AmeDigital_challenge_ios : NSObject
-@end
-@implementation PodsDummy_Pods_AmeDigital_challenge_ios
-@end
--- AmeDigital-challenge-ios/Pods/Target Support Files/Pods-AmeDigital-challenge-iosUITests/Pods-AmeDigital-challenge-iosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AmeDigital_challenge_iosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AmeDigital_challenge_iosUITests
-@end
